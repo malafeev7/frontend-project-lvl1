1fbc809efea08e0523ef662c295fe7dbb6ec3e78
Andrey Malafeyev <malafeev7@bk.ru>
2021-10-04 17:40:49 +0600

    lvl4

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
+
 const startBrainGames = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
-  };
+};
 
-  export default startBrainGames;
+export default startBrainGames;
